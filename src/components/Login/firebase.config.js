9fc31b861a9247d440eab35c5a9936537ad66844
Anis Islam <manisul1997bd@gmail.com>
2021-03-18 12:00:02 +0600

    private auth and redirect issues solved

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqXPJIxWpLZQkDyZoYPCNcD2YGXJEwjfQ",
+    authDomain: "burj-al-arab-23f78.firebaseapp.com",
+    projectId: "burj-al-arab-23f78",
+    storageBucket: "burj-al-arab-23f78.appspot.com",
+    messagingSenderId: "957939960836",
+    appId: "1:957939960836:web:96fb9f8375dd2a80c87951"
+  };
+
+  export default firebaseConfig;
